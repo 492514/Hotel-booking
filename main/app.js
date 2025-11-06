8af25b32c29e152d2cce0e67a04c05a219349ba4
shubham <vermashubham4925@gmail.com>
2025-11-06 13:57:10 +0530

    update

---
+++ main/app.js
@@ -0,0 +1,29 @@
+const login = document.querySelector("#login");
+const hotel = document.querySelector("#hotel");
+
+const register = document.querySelector("#registr");
+const explorebtn = document.querySelector(".explore");
+
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+login.addEventListener("click", function(){
+    window.location.assign("/Hotel/log-in/page.html");
+});
+
+register.addEventListener("click", function(){
+    window.location.assign("/Hotel/card/card.html");
+});
+
+hotel.addEventListener('click', function(){
+    window.location.assign("/Hotel/card/card.html");
+});
+
+explorebtn.addEventListener("click", function(){
+     window.location.assign("/Hotel/card/card.html");
+})
+
